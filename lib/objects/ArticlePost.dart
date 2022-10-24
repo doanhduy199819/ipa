6604d36c1fccf407d4ec8aad9738ababa3d7e80a
Duy Do <anhduy29121998@gmail.com>
2022-10-25 04:11:36 +0700

    Add DatabaseService
    Changes in ArticlePost(post_time => created_at, new methods for easy testing)

--- lib/objects/ArticlePost.dart
+++ lib/objects/ArticlePost.dart
@@ -3,76 +3,102 @@ import 'package:flutter_interview_preparation/objects/Account.dart';
 import 'Comment.dart';
 import 'package:intl/intl.dart';
 
-
 class ArticlePost {
   String? id;
   String? title;
-  DateTime? post_time;
+  DateTime? created_at;
   String? content;
-  List<String>? categories;
+  List<String> categories;
   String? author_id;
-  List<String>? liked_users;
-  List<Comment>? comments;
+  List<String> liked_users;
+  List<Comment> comments;
 
-  ArticlePost(
+  ArticlePost(this.id, this.title, this.created_at, this.content,
+      this.categories, this.author_id, this.liked_users, this.comments);
 
-        this.id,
-        this.title,
-       this.post_time,
-      this.content,
-        this.categories,
-        this.author_id,
-        this.liked_users,
-        this.comments
-           );
+  ArticlePost.only({
+    this.id,
+    this.title,
+    this.created_at,
+    this.content,
+    this.categories = const <String>[],
+    this.author_id,
+    this.liked_users = const <String>[],
+    this.comments = const <Comment>[],
+  });
+
+  factory ArticlePost.test() {
+    var id = 'id_test';
+    var comments = [
+      Comment(content: 'sample comment 1'),
+      Comment(content: 'sample comment 2')
+    ];
+    var content = 'sample content';
+    var created_at = DateTime.now();
+    var title = 'This is a test article';
+    return ArticlePost.only(
+      id: id,
+      comments: comments,
+      content: content,
+      created_at: created_at,
+      title: title,
+    );
+  }
+
+  void setId(String id) => this.id = id;
 
   factory ArticlePost.fromJson(Map<String, dynamic> data) {
     final String? id = data['id'] as String?;
     final String? title = data['title'] as String?;
 
-    final String? date_string_created = data['post_time'] as String?;
+    final String? date_string_created = data['created_at'] as String?;
     DateFormat formatter = DateFormat('dd/MM/yyyy');
-    final DateTime post_time =
-    formatter.parse(date_string_created ?? '1/1/2001');
+    final DateTime created_at =
+        formatter.parse(date_string_created ?? '1/1/2001');
 
-    final String? content= data['content'] as String;
+    final String? content = data['content'] as String;
     final List<String> categories = data['categories'] as List<String>;
-    final String? author_id= data['author_id'] as String;
+    final String? author_id = data['author_id'] as String;
     final List<String> liked_users = data['liked_users'] as List<String>;
     final List<Comment> comments = data['comments'] as List<Comment>;
 
-
-    return ArticlePost(id, title, post_time, content, categories, author_id, liked_users, comments);
+    return ArticlePost(id, title, created_at, content, categories, author_id,
+        liked_users, comments);
   }
 
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'title': title,
-    'post_time':post_time,
-    'content':content,
-    'categories': categories,
-    'author_id': author_id,
-    'liked_users': liked_users,
-    'comments': comments,
-  };
+        'id': id,
+        'title': title,
+        'post_time': created_at.toString(),
+        'content': content,
+        'categories': categories,
+        'author_id': author_id,
+        'liked_users': liked_users,
+        'comments': comments,
+      };
 
-  void addLikedUser(String userId) => liked_users!.add(userId);
+  void addLikedUser(String userId) => liked_users.add(userId);
 
-  void addCategories(String category)=>categories!.add(category);
+  void addCategories(String category) => categories.add(category);
 
   int get numberOfLile {
     return (liked_users?.length ?? 0);
   }
 
-static List<ArticlePost> getSampleArticlePostList() {
-    var listCategory=<String>['Algorithm','Java'];
-    var listAccount=<String>['id1','id2','id3'];
-    var listComment=Comment.getSampleCommentsList();
+  static List<ArticlePost> getSampleArticlePostList() {
+    var listCategory = <String>['Algorithm', 'Java'];
+    var listAccount = <String>['id1', 'id2', 'id3'];
+    var listComment = Comment.getSampleCommentsList();
     var _post = <ArticlePost>[];
-    _post.add(ArticlePost('1','What clothes we should use in the interview day',
+    _post.add(ArticlePost(
+        '1',
+        'What clothes we should use in the interview day',
         DateTime.now(),
-        'Clothes are one of the easiest impressive point to the interviewers',listCategory,'authorid1',listAccount,listComment
-    ));
+        'Clothes are one of the easiest impressive point to the interviewers',
+        listCategory,
+        'authorid1',
+        listAccount,
+        listComment));
 
     return _post;
   }
---
+++ lib/services/database_service.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_interview_preparation/objects/ArticlePost.dart';
+
+class DatabaseService {
+  // DatabaseReference ref = FirebaseDatabase.instance.ref();
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  List<ArticlePost>? _articlesListFromQuerySnapshot(
+      QuerySnapshot querySnapshot) {
+    return querySnapshot.docs.map((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        return ArticlePost.fromJson(
+            documentSnapshot.data() as Map<String, dynamic>);
+      }
+      return ArticlePost.test();
+    }).toList();
+  }
+
+  Stream<List<ArticlePost>?> get articlesList {
+    return db
+        .collection('articles')
+        .snapshots()
+        .map(_articlesListFromQuerySnapshot);
+  }
+
+  void testReadArticles() async {
+    await db.collection('company').get().then((event) {
+      for (var doc in event.docs) {
+        print("id: ${doc.id} => data: ${doc.data()}");
+      }
+    });
+  }
+
+  void addListArticles(List<ArticlePost> list) {
+    CollectionReference collection = db.collection('articles');
+    list.forEach((article) {
+      collection
+          .add(article.toJson())
+          .then((value) => print('Article added successfully'))
+          .catchError((onError) => print('Failed to add an article'));
+    });
+  }
+
+  void addArticle(ArticlePost article) {
+    // CollectionReference collection = db.collection('articles');
+    DocumentReference doc = db.collection('articles').doc();
+    String id = doc.id;
+    if (article.id == null) article.setId(id);
+    doc
+        .set(article.toJson())
+        .then((value) => print('Article added successfully'))
+        .catchError((error) => print('Failed to add an article'));
+  }
+
+  Future<List<ArticlePost>?> getArticlesList() async {
+    CollectionReference collection = db.collection('articles');
+    // return a QuerySnapshot, which is a collection query
+    // To access documents in a collection,
+    // querySnapshot.docs() => return a List<DocumentSnapshot>
+    List<ArticlePost>? result;
+    await collection.get().then((QuerySnapshot querySnapshot) {
+      // print(querySnapshot.docs.first.data());
+      result = querySnapshot.docs.map((doc) {
+        if (doc.exists) {
+          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+          ArticlePost a = ArticlePost.fromJson(data);
+          return a;
+        }
+        return ArticlePost.test();
+      }).toList();
+    });
+    return result;
+  }
+}
